734ae5ecb4eef320b90cd16fc5acd7798e1a7127
iago <iagodeaquino10@gmail.com>
2025-05-26 10:56:55 -0300

    inserted new api to manage css files manipulation

---
+++ src/css_manager.py
@@ -0,0 +1,66 @@
+
+from flask import Flask, request
+from flask_cors import CORS
+from glob import glob
+from pathlib import Path
+
+#Functions
+def get_complementary_text():
+    with open('./styles/globals.css', 'r') as file:
+        whole_text = file.read().replace("\n", "")
+
+    end_flag = True
+    position = 0
+
+    while end_flag:
+        if whole_text[position] == "}":
+            end_flag = False
+        position+=1
+
+    complementary_text = whole_text[position:]
+    return complementary_text
+
+def save_css(css_text, path):
+    with open(path, 'w') as file:
+        file.write(css_text)
+
+
+
+
+
+def mesclate_text(css_text_to_mesclate):
+    return f":root {{--primary-color:{css_text_to_mesclate['primary_color']};--secondary-color:{css_text_to_mesclate['secondary_color']};\
+    --third-color:{css_text_to_mesclate['third_color']};--primary-font-color: {css_text_to_mesclate['primary_font_color']};--secondary-font-color: {css_text_to_mesclate['secondary_font_color']};\
+    --title-weight: {css_text_to_mesclate['title_weight']};--title-size: {css_text_to_mesclate['title_size']};--title-font-family: {css_text_to_mesclate['title_font_family']};\
+    --main-text-weight: {css_text_to_mesclate['main_text_weight']};--font-size: {css_text_to_mesclate['font_size']};--normal-text-font-family: {css_text_to_mesclate['normal_text_font_family']};}}"
+
+def create_new_style(css_text, style_name):
+    save_folder = Path("saved_styles")
+    save_folder.mkdir(exist_ok=True)
+    save_css(css_text, f'{save_folder}/{style_name}.css')
+
+
+#Routes
+app = Flask("css_manager")
+CORS(app)
+@app.post("/merge")
+def merge_css():
+    request_response = request.get_json()
+    main_configuration = mesclate_text(request_response)
+    save_css(main_configuration+get_complementary_text(), "./styles/globals.css")
+    return ""
+
+@app.post("/save_new")
+def save_new_css():
+    request_response = request.get_json()
+    main_configuration = mesclate_text(request_response)
+    create_new_style(main_configuration+get_complementary_text(), request_response['style_name'])
+    return ""
+
+@app.get("/get_saved_files")
+def get_all_files_saved():
+    files = glob("saved_styles/*css")
+    files_name_list = []
+    for file in files:
+        files_name_list.append(file.split("\\")[1].split('.')[0])
+    return files_name_list
