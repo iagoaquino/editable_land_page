86873840becbcac333dddc0bc6e4c18b0f304d93
iago <iagodeaquino10@gmail.com>
2025-06-06 10:49:59 -0300

    css_manager moved out of src folder and new third font color configuration

--- css_manager.py
+++ css_manager.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 #Functions
 def get_complementary_text():
-    with open('../public/styles/globals.css', 'r') as file:
+    with open('public/styles/globals.css', 'r') as file:
         whole_text = file.read().replace("\n", "")
 
     end_flag = True
@@ -30,12 +30,13 @@ def save_css(css_text, path):
 
 def mesclate_text(css_text_to_mesclate):
     return f":root {{--primary-color:{css_text_to_mesclate['primary_color']};--secondary-color:{css_text_to_mesclate['secondary_color']};\
-    --third-color:{css_text_to_mesclate['third_color']};--primary-font-color: {css_text_to_mesclate['primary_font_color']};--secondary-font-color: {css_text_to_mesclate['secondary_font_color']};\
+    --third-color:{css_text_to_mesclate['third_color']}\
+    ;--primary-font-color: {css_text_to_mesclate['primary_font_color']};--secondary-font-color: {css_text_to_mesclate['secondary_font_color']};--third-font-color: {css_text_to_mesclate['third_font_color']};\
     --title-weight: {css_text_to_mesclate['title_weight']};--title-size: {css_text_to_mesclate['title_size']};--title-font-family: {css_text_to_mesclate['title_font_family']};\
     --main-text-weight: {css_text_to_mesclate['main_text_weight']};--font-size: {css_text_to_mesclate['font_size']};--normal-text-font-family: {css_text_to_mesclate['normal_text_font_family']};}}"
 
 def create_new_style(css_text, style_name):
-    save_folder = Path("../public/saved_styles/")
+    save_folder = Path("public/saved_styles/")
     save_folder.mkdir(exist_ok=True)
     save_css(css_text, f'{save_folder}/{style_name}.css')
 
@@ -47,7 +48,7 @@ CORS(app)
 def merge_css():
     request_response = request.get_json()
     main_configuration = mesclate_text(request_response)
-    save_css(main_configuration+get_complementary_text(), "../public/styles/globals.css")
+    save_css(main_configuration+get_complementary_text(), "public/styles/globals.css")
     return ""
 
 @app.post("/save_new")
@@ -59,7 +60,7 @@ def save_new_css():
 
 @app.get("/get_saved_files")
 def get_all_files_saved():
-    files = glob("../public/saved_styles/*css")
+    files = glob("public/saved_styles/*css")
     files_name_list = []
     for file in files:
         files_name_list.append(file.split("\\")[1].split('.')[0])
@@ -67,7 +68,7 @@ def get_all_files_saved():
 
 @app.delete("/delete/<name>")
 def delete_file(name=None):
-    file = Path('../public/saved_styles/').joinpath(f'{name}.css')
+    file = Path('public/saved_styles/').joinpath(f'{name}.css')
     print(file)
     if file.exists():
         file.unlink()
